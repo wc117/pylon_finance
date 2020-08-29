f5b33e982e3539bc894ce013317471a4bfa1adf7
fsdev59 <fullservicetalentdev@gmail.com>
2020-08-29 17:03:55 -0400

    disable castvote

--- contracts/governance/PYLONGovernorAlpha.sol
+++ contracts/governance/PYLONGovernorAlpha.sol
@@ -391,23 +391,24 @@ contract GovernorAlpha {
     )
         internal
     {
-        require(state(proposalId) == ProposalState.Active, "GovernorAlpha::_castVote: voting is closed");
-        Proposal storage proposal = proposals[proposalId];
-        Receipt storage receipt = proposal.receipts[voter];
-        require(receipt.hasVoted == false, "GovernorAlpha::_castVote: voter already voted");
-        uint256 votes = pylon.getPriorVotes(voter, proposal.startBlock);
+        // disable vote
+        // require(state(proposalId) == ProposalState.Active, "GovernorAlpha::_castVote: voting is closed");
+        // Proposal storage proposal = proposals[proposalId];
+        // Receipt storage receipt = proposal.receipts[voter];
+        // require(receipt.hasVoted == false, "GovernorAlpha::_castVote: voter already voted");
+        // uint256 votes = pylon.getPriorVotes(voter, proposal.startBlock);
 
-        if (support) {
-            proposal.forVotes = add256(proposal.forVotes, votes);
-        } else {
-            proposal.againstVotes = add256(proposal.againstVotes, votes);
-        }
+        // if (support) {
+        //     proposal.forVotes = add256(proposal.forVotes, votes);
+        // } else {
+        //     proposal.againstVotes = add256(proposal.againstVotes, votes);
+        // }
 
-        receipt.hasVoted = true;
-        receipt.support = support;
-        receipt.votes = votes;
+        // receipt.hasVoted = true;
+        // receipt.support = support;
+        // receipt.votes = votes;
 
-        emit VoteCast(voter, proposalId, support, votes);
+        // emit VoteCast(voter, proposalId, support, votes);
     }
 
     function __acceptAdmin()
